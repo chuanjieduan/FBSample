b4b32085e3535801e785be088e5fc431bb93cebf
cjduan <cjduan@ctrip.com>
2015-01-27 22:52:14 +0800

    deleteall

--- main/java/sk/viawebtour/www/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package sk.viawebtour.www.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sk.viawebtour.www.domain.User;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
--- main/java/sk/viawebtour/www/service/excepiton/UserAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package sk.viawebtour.www.service.excepiton;
-
-public class UserAlreadyExistsException extends RuntimeException {
-
-    public UserAlreadyExistsException(final String message) {
-        super(message);
-    }
-
-}
